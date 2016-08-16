a6599df01ba1e72266cf1873a7dad7ceba0addb9
Alex Lin <Alex_vii@live.com>
2016-08-16 21:52:00 +0800

    Add DatePickerFragment and TimePickerFragment

---
+++ app/src/main/java/com/poipoipo/timeline/DatePickerFragment.java
@@ -0,0 +1,45 @@
+package com.poipoipo.timeline;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+    private static final String YEAR = "year";
+    private static final String MONTH = "month";
+    private static final String DAY = "day";
+    Calendar calendar = Calendar.getInstance();
+
+    public static DatePickerFragment newInstance(Calendar calendar) {
+        Bundle args = new Bundle();
+        args.putInt(YEAR, calendar.get(Calendar.YEAR));
+        args.putInt(MONTH, calendar.get(Calendar.MONTH));
+        args.putInt(DAY, calendar.get(Calendar.DAY_OF_MONTH));
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        if (calendar == null) {
+            calendar.set(Calendar.YEAR, getArguments().getInt(YEAR));
+            calendar.set(Calendar.MONTH, getArguments().getInt(MONTH));
+            calendar.set(Calendar.DAY_OF_MONTH, getArguments().getInt(DAY));
+        }
+        return new DatePickerDialog(getActivity(), this, calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+        calendar.set(Calendar.YEAR, i);
+        calendar.set(Calendar.MONTH, i1);
+        calendar.set(Calendar.DAY_OF_MONTH, i2);
+    }
+}
